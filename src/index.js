112776bfe64292939451a0c4109054c10a904567
Divakar <divaakaar.saakthi@gmail.com>
2023-02-25 19:24:50 +0530

    added tweet service with hastag processing

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ const express = require('express');
 const connect  = require('./config/database');
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository')
+const TweetRepository = require('./repository/tweet-repository');
+const TweetService  = require('./services/tweet-service');
 const Comment = require('./model/comment');
 
 app.listen(3000, async () => {
     console.log('Server Started');
     await connect();
-    const tweetRepo = new TweetRepository();
+    const service = new TweetService();
     // tweetRepo.create({
     //     content : 'This is 1st tweet',
     //     userEmail: 'a@b.com'
     // })
-    // tweetRepo.create({
-    //     content : 'This is 2ndtweet',
-    //     userEmail: 'c@d.com'
-    // })
-    const t=await tweetRepo.getAll(1,3);
-    console.log(t);
+    service.create({
+        content : 'This is 6th #tweet and #coding is necessary',
+        // userEmail: 'c@d.com'
+    })
+    // const t=await tweetRepo.getAll(1,3);
+    // console.log(t);
 })
